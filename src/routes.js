d27009ea2a406f9c8e0d4861c2bf2131012b46a7
Sammuel Reis <sammugr@hotmail.com>
2020-03-12 20:05:18 -0300

    implements model loader

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello, World' }));
+routes.get('/', async (req, res) => {
+	const user = await User.create({
+		name: 'Sammuel Reis',
+		email: 'sammuel@email.com.br',
+		password_hash: '123123',
+	});
+
+	return res.json(user);
+});
 
 export default routes;
