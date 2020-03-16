3f157310dd3bd2e802889b27b438a3dbc46f223a
Sammuel Reis <sammugr@hotmail.com>
2020-03-15 22:34:02 -0300

    implements auth middleware

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -17,6 +17,12 @@ class UserController {
 			admin,
 		});
 	}
+
+	async update(req, res) {
+		console.log(req.userId);
+
+		return res.json({ ok: true });
+	}
 }
 
 export default new UserController();
