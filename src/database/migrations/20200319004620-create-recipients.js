c717ebea9e7791bd0df28c9bd11e02069f4e011c
Sammuel Reis <sammugr@hotmail.com>
2020-03-18 22:24:09 -0300

    implements recipients migration

---
+++ src/database/migrations/20200319004620-create-recipients.js
@@ -0,0 +1,51 @@
+module.exports = {
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable('recipients', {
+			id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+			},
+			name: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			adress_street: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			adress_number: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+			},
+			adress_complement: {
+				type: Sequelize.STRING,
+			},
+			adress_state: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			adress_city: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			adress_zip_code: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			created_at: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+			updated_at: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+		});
+	},
+
+	down: queryInterface => {
+		return queryInterface.dropTable('users');
+	},
+};
